cb31e5f261629ae68ccb955f8a2391704b210e09
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-04-26 12:39:05 -0700

    Update PlayerController.cs

--- Project Miles/Assets/Scripts/PlayerController.cs
+++ Project Miles/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float rotationSpeed;
+    public float jumpSpeed;
+
     private CharacterController characterController;
+    private float ySpeed;
+    //1:17 https://www.youtube.com/watch?v=ynh7b-AUSPE&list=PLx7AKmQhxJFaj0IcdjGJzIq5KwrIfB1m9&index=6&ab_channel=KetraGames
 
     void Start()
     {
@@ -20,8 +24,8 @@ public class PlayerController : MonoBehaviour
             float verticalInput = Input.GetAxis("Vertical");
 
             Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;
-        //movementDirection.Normalize(); <--- grid movement
+            float magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;   
+            //movementDirection.Normalize(); <--- grid movement
 
         //transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
         characterController.SimpleMove(movementDirection * magnitude);
