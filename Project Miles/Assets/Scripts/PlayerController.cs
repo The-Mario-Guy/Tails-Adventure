6e7cb8919083d27aedce5188050c1602be4c0b32
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-04-26 12:13:09 -0700

    Working on character movement

--- Project Miles/Assets/Scripts/PlayerController.cs
+++ Project Miles/Assets/Scripts/PlayerController.cs
@@ -5,37 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    //public Rigidbody _RB;
-    public float jumpForce;
-    public CharacterController controller;
-
-    private Vector3 moveDirection;
-    public float gravityScale;
 
 
     void Start()
     {
-        //_RB = GetComponent<Rigidbody>();
-        controller = GetComponent<CharacterController>();
+
+
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // _RB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, _RB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-
-        // if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //     _RB.velocity = new Vector3(_RB.velocity.x, jumpForce, _RB.velocity.z);
-        // }
-
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
-
-        if (Input.GetKeyDown(KeyCode.Z))
+        // Update is called once per frame
+        void Update()
         {
-            moveDirection.y - jumpForce;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+            //movementDirection.Normalize(); <--- grid movement
+
+            transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
         }
-        moveDirection.y = moveDirection.y * (Physics.gravity.y * gravityScale);
-        controller.Move(moveDirection * Time.deltaTime);
-    }
+    
 }
